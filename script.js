63296935842ced9b5fc86daf5f7d54cf340948c0
randomstump <67879187+randomstump@users.noreply.github.com>
2022-11-29 00:48:51 -0500

    Set up basic HTML to have chosen player and enemy fighter + benches to be separate. Set js to easily access specific team array fighters

--- script.js
+++ script.js
@@ -29,5 +29,24 @@ const knight = new Fighter("Knight", 1, 1, 1, 1, 1)
 const player = [barbarian, swashbuckler, ranger, assassin, wizard, healer, monk, rogue, knight]
 const computer = [barbarian, swashbuckler, ranger, assassin, wizard, healer, monk, rogue, knight]
 
-console.log(player)
-console.log(computer)
+// Easily access array items and distinguish between teams
+const pbar = player[0]
+const pswa = player[1]
+const pran = player[2]
+const pass = player[3]
+const pwiz = player[4]
+const phea = player[5]
+const pmon = player[6]
+const prog = player[7]
+const pkni = player[8]
+
+const cbar = computer[0]
+const cswa = computer[1]
+const cran = computer[2]
+const cass = computer[3]
+const cwiz = computer[4]
+const chea = computer[5]
+const cmon = computer[6]
+const crog = computer[7]
+const ckni = computer[8]
+
