1aedfffd45cfceffca1ba3e67fa0bd124cdbaad3
randomstump <67879187+randomstump@users.noreply.github.com>
2022-11-29 01:07:18 -0500

    Added skills for each class

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // Base class set up
 class Fighter {
-    constructor(profession ,health, attack, dexterity, wisdom, defense) {
+    constructor(profession ,health, attack, dexterity, wisdom, defense,skill1, skill2, skill3) {
         this.profession = profession
         this.health = health
         this.attack = attack
         this.dexterity = dexterity
         this.wisdom = wisdom
         this.defense = defense
+        this.skill1 =skill1
+        this.skill2 =skill2
+        this.skill3 =skill3
     }
     attack() {
         const roll = Math.floor(Math.random() * 6)
@@ -15,15 +18,15 @@ class Fighter {
 }
 
 // Generated Classes
-const barbarian = new Fighter("Barbarian", 1, 1, 1, 1, 1)
-const swashbuckler = new Fighter("Swashbuckler", 1, 1, 1, 1, 1)
-const ranger = new Fighter("Ranger", 1, 1, 1, 1, 1)
-const assassin = new Fighter("Assassin", 1, 1, 1, 1, 1)
-const wizard = new Fighter("Wizard", 1, 1, 1, 1, 1)
-const healer = new Fighter("Healer", 1, 1, 1, 1, 1)
-const monk = new Fighter("Monk", 1, 1, 1, 1, 1)
-const rogue = new Fighter("Rogue", 1, 1, 1, 1, 1)
-const knight = new Fighter("Knight", 1, 1, 1, 1, 1)
+const barbarian = new Fighter("Barbarian", 1, 1, 1, 1, 1, "Swing more","Rage On","Cleave")
+const swashbuckler = new Fighter("Swashbuckler", 1, 1, 1, 1, 1, "Stab", "Slice", "Weak Spot")
+const ranger = new Fighter("Ranger", 1, 1, 1, 1, 1, "Double Shot", "Split Target", "Precise Shot")
+const assassin = new Fighter("Assassin", 1, 1, 1, 1, 1, "Backstab", "Throwing Knives", "Deadly Strike")
+const wizard = new Fighter("Wizard", 1, 1, 1, 1, 1, "Fireball", "Shock", "Hail")
+const healer = new Fighter("Healer", 1, 1, 1, 1, 1, "Heal", "Boost", "Smite")
+const monk = new Fighter("Monk", 1, 1, 1, 1, 1, "Precise Attack", "Flying Kick", "Triple Unarmed Strike")
+const rogue = new Fighter("Rogue", 1, 1, 1, 1, 1, "Slight of Hand", "Trained Eye", "Stroke of Luck")
+const knight = new Fighter("Knight", 1, 1, 1, 1, 1, "Sheild Bash", "Strike Down", "Intimidate")
 
 // Each side set with arrays
 const player = [barbarian, swashbuckler, ranger, assassin, wizard, healer, monk, rogue, knight]
