c3e3cd65eaf73bacca796fc341dd26a068b65fc9
randomstump <67879187+randomstump@users.noreply.github.com>
2022-12-01 22:39:43 -0500

    Add imgs to class, add img to html and selected on js, add lists to modal, assigned each portrait to the lists, made list portraits clickable only one, tested assigning innertext and imgs for fighters to html

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 // Base class set up
 class Fighter {
-    constructor(profession ,health, attack, dexterity, wisdom, defense,skill1, skill2, skill3) {
+    constructor(profession ,health, strength, dexterity, wisdom, defense,skill1, skill2, skill3, img) {
         this.profession = profession
         this.health = health
-        this.attack = attack
+        this.strength = strength
         this.dexterity = dexterity
         this.wisdom = wisdom
         this.defense = defense
         this.skill1 = skill1
         this.skill2 = skill2
         this.skill3 = skill3
+        this.img = img
     }
-    attackroll() {
+    strengthroll() {
         const roll = Math.floor(Math.random() * 6)
         return roll
     }
 }
 
 // Generated Classes
-const barbarian = new Fighter("Barbarian", 1, 1, 1, 1, 1, "Swing more","Rage On","Cleave")
-const swashbuckler = new Fighter("Swashbuckler", 1, 1, 1, 1, 1, "Stab", "Slice", "Weak Spot")
-const ranger = new Fighter("Ranger", 1, 1, 1, 1, 1, "Double Shot", "Split Target", "Precise Shot")
-const assassin = new Fighter("Assassin", 1, 1, 1, 1, 1, "Backstab", "Throwing Knives", "Deadly Strike")
-const wizard = new Fighter("Wizard", 1, 1, 1, 1, 1, "Fireball", "Shock", "Hail")
-const healer = new Fighter("Healer", 1, 1, 1, 1, 1, "Heal", "Boost", "Smite")
-const monk = new Fighter("Monk", 1, 1, 1, 1, 1, "Precise Attack", "Flying Kick", "Triple Unarmed Strike")
-const rogue = new Fighter("Rogue", 1, 1, 1, 1, 1, "Slight of Hand", "Trained Eye", "Stroke of Luck")
-const knight = new Fighter("Knight", 1, 1, 1, 1, 1, "Sheild Bash", "Strike Down", "Intimidate")
+const barbarian = new Fighter("Barbarian", 15, 8, 5, 2, 7, "SWING MORE","RAGE ON","CLEAVE", "https://i.imgur.com/DRUAaya.png")
+const swashbuckler = new Fighter("Swashbuckler", 12, 5, 6, 1, 9, "STAB", "SLICE", "WEAK SPOT", "https://i.imgur.com/VdFaN2C.png")
+const ranger = new Fighter("Ranger", 12, 3, 9, 1, 3, "DOUBE SHOT", "SPLIT TARGET", "PRECISE SHOT", "https://i.imgur.com/aKAkHel.png")
+const assassin = new Fighter("Assassin", 10, 2, 11, 1, 5, "BACKSTAB", "THROWING KNIVES", "DEADLY STRIKE", "https://i.imgur.com/Of4M4A0.png")
+const wizard = new Fighter("Wizard", 8, 1, 3, 1, 2, "FIREBALL", "SHOCK", "HAIL", "https://i.imgur.com/AWH3C1X.png")
+const healer = new Fighter("Healer", 10, 1, 4, 1, 3, "HEAL", "BOOST", "SMITE", "https://i.imgur.com/4XxzquY.png")
+const monk = new Fighter("Monk", 12, 7, 7, 1, 8, "PRECISE strength", "FLYING KICK", "TRIPLE UNARMED STRIKE", "https://i.imgur.com/5twXEfM.png")
+const rogue = new Fighter("Rogue", 10, 4, 9, 1, 6, "SLEIGHT OF HAND", "TRAINED EYE", "STROKE OF LUCK", "https://i.imgur.com/04Jy4eo.png")
+const knight = new Fighter("Knight", 30, 3, 3, 1, 13, "SHEILD BASH", "STRIKE DOWN", "INTIMIDATE", "https://i.imgur.com/vVPsT2g.png")
 
 // Each side set with arrays
 const player1 = [barbarian, swashbuckler, ranger, assassin, wizard, healer, monk, rogue, knight]
 const player2 = [barbarian, swashbuckler, ranger, assassin, wizard, healer, monk, rogue, knight]
 
-const player1selected = []
-const player2selected = []
+const p1s= []
+const p2s = []
 
 // Easily access array items and distinguish between teams
 const p1bar = player1[0]
@@ -56,65 +57,71 @@ const p2mon = player2[6]
 const p2rog = player2[7]
 const p2kni = player2[8]
 
-const posit11 = document.querySelector('.p11')
-const p11h = document.querySelector('.p11 > .health')
-const p11a = document.querySelector('.p11 > .attack')
-const p11d = document.querySelector('.p11 > .dexterity')
-const p11w = document.querySelector('.p11 > .wisdom')
-const p11def = document.querySelector('.p11 > .defense')
+const posit11 = document.querySelector('.p11 > .profession')
+const p11h = document.querySelector('.p11 > .health > span')
+const p11a = document.querySelector('.p11 > .strength > span')
+const p11d = document.querySelector('.p11 > .dexterity > span')
+const p11w = document.querySelector('.p11 > .wisdom > span')
+const p11def = document.querySelector('.p11 > .defense > span')
 const p11hs1 = document.querySelector('.p11 > .skill1')
 const p11hs2 = document.querySelector('.p11 > .skill2')
 const p11hs3 = document.querySelector('.p11 > .skill3')
+const p11i = document.querySelector('.p11 > img')
 
-const posit12 = document.querySelector('.p12')
-const p12h = document.querySelector('.p12 > .health')
-const p12a = document.querySelector('.p12 > .attack')
-const p12d = document.querySelector('.p12 > .dexterity')
-const p12w = document.querySelector('.p12 > .wisdom')
-const p12def = document.querySelector('.p12 > .defense')
+const posit12 = document.querySelector('.p12 > .profession')
+const p12h = document.querySelector('.p12 > .health > span')
+const p12a = document.querySelector('.p12 > .strength > span')
+const p12d = document.querySelector('.p12 > .dexterity > span')
+const p12w = document.querySelector('.p12 > .wisdom > span')
+const p12def = document.querySelector('.p12 > .defense > span')
 const p12hs1 = document.querySelector('.p12 > .skill1')
 const p12hs2 = document.querySelector('.p12 > .skill2')
 const p12hs3 = document.querySelector('.p12 > .skill3')
+const p12i = document.querySelector('.p12 > img')
 
-const posit13 = document.querySelector('.p13')
-const p13h = document.querySelector('.p13 > .health')
-const p13a = document.querySelector('.p13 > .attack')
-const p13d = document.querySelector('.p13 > .dexterity')
-const p13w = document.querySelector('.p13 > .wisdom')
-const p13def = document.querySelector('.p13 > .defense')
+const posit13 = document.querySelector('.p13 > .profession')
+const p13h = document.querySelector('.p13 > .health > span')
+const p13a = document.querySelector('.p13 > .strength > span')
+const p13d = document.querySelector('.p13 > .dexterity > span')
+const p13w = document.querySelector('.p13 > .wisdom > span')
+const p13def = document.querySelector('.p13 > .defense > span')
 const p13hs1 = document.querySelector('.p13 > .skill1')
 const p13hs2 = document.querySelector('.p13 > .skill2')
 const p13hs3 = document.querySelector('.p13 > .skill3')
+const p13i = document.querySelector('.p13 > img')
 
-const posit21 = document.querySelector('.p21')
-const p21h = document.querySelector('.p21 > .health')
-const p21a = document.querySelector('.p21 > .attack')
-const p21d = document.querySelector('.p21 > .dexterity')
-const p21w = document.querySelector('.p21 > .wisdom')
-const p21def = document.querySelector('.p21 > .defense')
+const posit21 = document.querySelector('.p21 > .profession')
+const p21h = document.querySelector('.p21 > .health > span')
+const p21a = document.querySelector('.p21 > .strength > span')
+const p21d = document.querySelector('.p21 > .dexterity > span')
+const p21w = document.querySelector('.p21 > .wisdom > span')
+const p21def = document.querySelector('.p21 > .defense > span')
 const p21hs1 = document.querySelector('.p21 > .skill1')
 const p21hs2 = document.querySelector('.p21 > .skill2')
 const p21hs3 = document.querySelector('.p21 > .skill3')
+const p21i = document.querySelector('.p21 > img')
 
-const posit22 = document.querySelector('.p22')
-const p22h = document.querySelector('.p22 > .health')
-const p22a = document.querySelector('.p22 > .attack')
-const p22d = document.querySelector('.p22 > .dexterity')
-const p22w = document.querySelector('.p22 > .wisdom')
-const p22def = document.querySelector('.p22 > .defense')
+const posit22 = document.querySelector('.p22 > .profession')
+const p22h = document.querySelector('.p22 > .health > span')
+const p22a = document.querySelector('.p22 > .strength > span')
+const p22d = document.querySelector('.p22 > .dexterity > span')
+const p22w = document.querySelector('.p22 > .wisdom > span')
+const p22def = document.querySelector('.p22 > .defense > span')
 const p22hs1 = document.querySelector('.p22 > .skill1')
 const p22hs2 = document.querySelector('.p22 > .skill2')
 const p22hs3 = document.querySelector('.p22 > .skill3')
+const p22i = document.querySelector('.p22 > img')
 
-const posit23 = document.querySelector('.p23')
-const p23h = document.querySelector('.p23 > .health')
-const p23a = document.querySelector('.p23 > .attack')
-const p23d = document.querySelector('.p23 > .dexterity')
-const p23w = document.querySelector('.p23 > .wisdom')
-const p23def = document.querySelector('.p23 > .defense')
+const posit23 = document.querySelector('.p23 > .profession')
+const p23h = document.querySelector('.p23 > .health > span')
+const p23a = document.querySelector('.p23 > .strength > span')
+const p23d = document.querySelector('.p23 > .dexterity > span')
+const p23w = document.querySelector('.p23 > .wisdom > span')
+const p23def = document.querySelector('.p23 > .defense > span')
 const p23hs1 = document.querySelector('.p23 > .skill1')
 const p23hs2 = document.querySelector('.p23 > .skill2')
 const p23hs3 = document.querySelector('.p23 > .skill3')
+const p23i = document.querySelector('.p23 > img')
 
 const p1b1 = document.querySelector('#player1bench > .b1')
 const p1b2 = document.querySelector('#player1bench > .b2')
@@ -130,26 +137,169 @@ const p2b4 = document.querySelector('#player2bench > .b4')
 const p2b5 = document.querySelector('#player2bench > .b5')
 const p2b6 = document.querySelector('#player2bench > .b6')
 
-const p11i = document.querySelector('.p11 > img')
-const p12i = document.querySelector('.p12 > img')
-const p13i = document.querySelector('.p13 > img')
+const li10 = document.querySelector('.p1list > .l0 > img')
+const li11 = document.querySelector('.p1list > .l1 > img')
+const li12 = document.querySelector('.p1list > .l2 > img')
+const li13 = document.querySelector('.p1list > .l3 > img')
+const li14 = document.querySelector('.p1list > .l4 > img')
+const li15 = document.querySelector('.p1list > .l5 > img')
+const li16 = document.querySelector('.p1list > .l6 > img')
+const li17 = document.querySelector('.p1list > .l7 > img')
+const li18 = document.querySelector('.p1list > .l8 > img')
 
-const img = document.querySelectorAll("img")
-console.log(img)
+const li20 = document.querySelector('.p2list > .l0 > img')
+const li21 = document.querySelector('.p2list > .l1 > img')
+const li22 = document.querySelector('.p2list > .l2 > img')
+const li23 = document.querySelector('.p2list > .l3 > img')
+const li24 = document.querySelector('.p2list > .l4 > img')
+const li25 = document.querySelector('.p2list > .l5 > img')
+const li26 = document.querySelector('.p2list > .l6 > img')
+const li27 = document.querySelector('.p2list > .l7 > img')
+const li28 = document.querySelector('.p2list > .l8 > img')
+
+li10.src = p1bar.img
+li11.src = p1swa.img
+li12.src = p1ran.img
+li13.src = p1ass.img
+li14.src = p1wiz.img
+li15.src = p1hea.img
+li16.src = p1mon.img
+li17.src = p1rog.img
+li18.src = p1kni.img
+
+li20.src = p2bar.img
+li21.src = p2swa.img
+li22.src = p2ran.img
+li23.src = p2ass.img
+li24.src = p2wiz.img
+li25.src = p2hea.img
+li26.src = p2mon.img
+li27.src = p2rog.img
+li28.src = p2kni.img
+
 posit11.innerText = p1bar.profession
+p11h.innerText = p1bar.health
+p11a.innerText = p1bar.strength
+p11d.innerText = p1bar.dexterity
+p11w.innerText = p1bar.wisdom
+p11def.innerText = p1bar.defense
+p11i.src = p1bar.img
+p11hs1.innerText = p1bar.skill1
+p11hs2.innerText = p1bar.skill2
+p11hs3.innerText = p1bar.skill3
 
-img[0].src = "https://i.imgur.com/DRUAaya.png"
-img[0].style = "width:50px;height:50px"
-console.log(img[0])
+posit21.innerText = p2bar.profession
+p21h.innerText = p2bar.health
+p21a.innerText = p2bar.strength
+p21d.innerText = p2bar.dexterity
+p21w.innerText = p2bar.wisdom
+p21def.innerText = p2bar.defense
+p21i.src = p2bar.img
+p21hs1.innerText = p2bar.skill1
+p21hs2.innerText = p2bar.skill2
+p21hs3.innerText = p2bar.skill3
+
+
+
+
 
 
 //functions
 
-// function attack() {
 
-// }
+//Event Listener
 
+li10.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1bar)
+    console.log(p1s)
+}, {once : true})
+li11.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1swa)
+    console.log(p1s)
+}, {once : true})
+li12.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1ran)
+    console.log(p1s)
+}, {once : true})
+li13.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1ass)
+    console.log(p1s)
+}, {once : true})
+li14.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1wiz)
+    console.log(p1s)
+}, {once : true})
+li15.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1hea)
+    console.log(p1s)
+}, {once : true})
+li16.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1mon)
+    console.log(p1s)
+}, {once : true})
+li17.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1rog)
+    console.log(p1s)
+}, {once : true})
+li18.addEventListener('click', () => {
+    console.log("clicked")
+    p1s.push(p1kni)
+    console.log(p1s)
+}, {once : true})
 
+li20.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2bar)
+    console.log(p2s)
+}, {once : true})
+li21.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2swa)
+    console.log(p2s)
+}, {once : true})
+li22.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2ran)
+    console.log(p2s)
+}, {once : true})
+li23.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2ass)
+    console.log(p2s)
+}, {once : true})
+li24.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2wiz)
+    console.log(p2s)
+}, {once : true})
+li25.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2hea)
+    console.log(p2s)
+}, {once : true})
+li26.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2mon)
+    console.log(p2s)
+}, {once : true})
+li27.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2rog)
+    console.log(p2s)
+}, {once : true})
+li28.addEventListener('click', () => {
+    console.log("clicked")
+    p2s.push(p2kni)
+    console.log(p2s)
+}, {once : true})
 
 // Modal
 const modal = document.getElementById('myModal')
